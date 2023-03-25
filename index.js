59f996b38e0c6c248eb1a801bad4aa259e2bfc35
Kendrick Villaruel <kendrick.villaruel@gmail.com>
2023-03-25 14:18:10 +0800

    Attempt to blink LED

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-console.log('Hello World');
-console.log('Hello World2');
+const Gpio = require('onoff').Gpio;
+const led = new Gpio(24, 'out');
+
+function blinkLED() {
+  if (led.readSync() === 0) {
+    led.writeSync(1);
+  } else {
+    led.writeSync(0);
+  }
+}
+
+setInterval(blinkLED, 500);
+
+process.on('SIGINT', () => {
+  clearInterval();
+  led.writeSync(0);
+  led.unexport();
+  console.log('LED stopped blinking and the program exited');
+  process.exit(0);
+});
